74aac0e9c6cbd0786a47ae9593c863db33b07a35
savchuckvadim <savchuckvadim@gmail.com>
2025-04-22 20:56:47 +0300

    fxd size

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './core/filters/global-exception.filter';
 import { ResponseInterceptor } from './core/interceptors/response.interceptor';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -21,6 +22,11 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(app.get(GlobalExceptionFilter));
   // app.enableCors();
+
+   // Увеличиваем лимит тела запроса (например, до 50MB)
+   app.use(bodyParser.json({ limit: '150mb' }));
+   app.use(bodyParser.urlencoded({ limit: '150mb', extended: true }));
+ 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
