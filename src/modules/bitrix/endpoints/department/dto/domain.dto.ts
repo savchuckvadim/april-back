9a3c04e1478b577abc26f7161198adf83eaecd22
savchuckvadim <savchuckvadim@gmail.com>
2025-05-19 14:11:52 +0300

    fxd domain gsr

--- src/modules/bitrix/endpoints/department/dto/domain.dto.ts
+++ src/modules/bitrix/endpoints/department/dto/domain.dto.ts
@@ -5,7 +5,7 @@ import { IsEnum } from "class-validator";
 export enum EClients {
     dev='april-dev.bitrix24.ru',
     april='april-garant.bitrix24.ru',
-    gsr='gsr-garant.bitrix24.ru',
+    gsr='gsr.bitrix24.ru',
     gsirk='gsirk.bitrix24.ru',
     alfacentr='alfacentr.bitrix24.ru',
 
