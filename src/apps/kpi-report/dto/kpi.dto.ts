8f2db3b89a28603db1659e98e30eb99b77d8251c
savchuckvadim <savchuckvadim@gmail.com>
2025-05-25 17:38:13 +0300

    fxd get report

--- src/apps/kpi-report/dto/kpi.dto.ts
+++ src/apps/kpi-report/dto/kpi.dto.ts
@@ -1,21 +1,34 @@
 
+import { Type } from "class-transformer";
+import { IsEnum, ValidateNested } from "class-validator";
 import { IBXUser } from "src/modules/bitrix/domain/interfaces/bitrix.interface";
 import { IFieldItem } from "src/modules/portal/interfaces/portal.interface";
 
-export class KpiReportDto {
-    type: EDownloadType
-    report: ReportData[]
-    date: {
-        from: string
-        to: string
-    }
 
-
-}
 export enum EDownloadType {
     EXCEL = 'excel',
     PDF = 'pdf',
 }
+
+export class DateRangeDto {
+    from: string;
+    to: string;
+}
+
+export class KpiReportDto {
+
+    @IsEnum(EDownloadType)
+    type: EDownloadType;
+    report: ReportData[]
+
+
+    @Type(() => DateRangeDto)
+    @ValidateNested()
+    date: DateRangeDto;
+
+
+}
+
 
 export interface ReportData {
     user: IBXUser;
@@ -61,7 +74,7 @@ export type FilterInnerCode = 'result_communication_done' |
     'result_communication_plan' | 'call_plan' | 'call_expired' | 'call_done' | 'call_pound' | 'call_act_noresult_fail' |
     'presentation_plan' | 'presentation_expired' | 'presentation_done' | 'presentation_pound' | 'presentation_act_noresult_fail' |
     'presentation_uniq_plan' | 'presentation_uniq_expired' | 'presentation_uniq_done' | 'presentation_uniq_pound' | 'presentation_uniq_act_noresult_fail' |
-    'presentation_contact_uniq_plan' |  'presentation_contact_uniq_done' | 
+    'presentation_contact_uniq_plan' | 'presentation_contact_uniq_done' |
     'ev_offer_act_send' | 'ev_offer_pres_act_send' | 'ev_invoice_act_send' | 'ev_invoice_pres_act_send' | 'ev_contract_act_send' | 'ev_success_done' |
     'ev_fail_done'
 
