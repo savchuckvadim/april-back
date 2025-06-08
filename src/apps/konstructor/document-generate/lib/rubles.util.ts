b6f80dea55a977e6a2924ca184a3b13e1937eed3
savchuckvadim <savchuckvadim@gmail.com>
2025-06-08 17:04:11 +0300

    fresh glob

--- src/apps/konstructor/document-generate/lib/rubles.util.ts
+++ src/apps/konstructor/document-generate/lib/rubles.util.ts
@@ -1,4 +1,6 @@
 import { rubles } from 'rubles';
+import { convert as convertNumberToWordsRu } from 'number-to-words-ru'
+//todo https://www.npmjs.com/package/number-to-words-ru
 export const formatRuble = (value: number): string => {
     // return new Intl.NumberFormat('ru-RU', {
     //     style: 'currency',
@@ -7,3 +9,27 @@ export const formatRuble = (value: number): string => {
     // }).format(value);
     return rubles(value)
 }
+
+// Использование без опций
+// convertNumberToWordsRu('104')
+// // Сто четыре рубля 00 копеек
+
+// // или с опциями
+// convertNumberToWordsRu('-4201512.21', {
+//   currency: 'rub',
+//   declension: 'nominative',
+//   roundNumber: -1,
+//   convertMinusSignToWord: true,
+//   showNumberParts: {
+//     integer: true,
+//     fractional: true,
+//   },
+//   convertNumberToWords: {
+//     integer: true,
+//     fractional: false,
+//   },
+//   showCurrency: {
+//     integer: true,
+//     fractional: true,
+//   },
+// })
