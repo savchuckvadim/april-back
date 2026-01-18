3bd94e46f687bcd94a4ce35b5a21727ced43d44a
Vadim Savchuk <savchuckvadim@gmail.com>
2026-01-18 15:34:32 +0300

    fx redis config

--- src/core/redis/redis.config.ts
+++ src/core/redis/redis.config.ts
@@ -4,7 +4,7 @@ import { ConfigService } from '@nestjs/config';
 export function createRedisOptions(
     configService: ConfigService,
 ): {
-   
+
     url: string | undefined,
     host: string | undefined,
     port: number, maxRetriesPerRequest: number,
