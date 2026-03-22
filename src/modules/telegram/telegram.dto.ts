af3b6abbb8bf101803edcec6d632c844ab76d907
Vadim Savchuk <savchuckvadim@gmail.com>
2026-03-22 18:33:21 +0300

    delete old tg dto

--- src/modules/telegram/telegram.dto.ts
+++ src/modules/telegram/telegram.dto.ts
@@ -1,32 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-// export enum EnumTelegramApp {
-//     KPI_SALES = 'kpi_sales',
-//     KONSTRUKTOR = 'konstruktor',
-// }
-
-export class TelegramSendMessageDto {
-    @ApiProperty({ description: 'Application name', type: String })
-    @IsString()
-    app: string;
-
-    @ApiProperty({ description: 'Text message' })
-    @IsString()
-    @IsNotEmpty()
-    text: string;
-
-    @ApiProperty({ description: 'Domain', example: 'example.bitrix24.ru' })
-    @IsString()
-    @IsNotEmpty()
-    domain: string;
-
-    @ApiProperty({ description: 'User ID' })
-    @IsString()
-    @IsNotEmpty()
-    userId: string;
-}
-
 export class TelegramSendMessagePublicDto {
     @ApiProperty({ description: 'Application name', type: String })
     @IsString()
